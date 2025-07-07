11d743ba09bb3b52624f9d56b06c0fe748129678
Lionheadgithub <lionhead20110502@gmail.com>
2025-07-07 18:01:38 +0900

    レビュー指摘箇所を修正

--- python/golf_score.py
+++ python/golf_score.py
@@ -1,24 +1,19 @@
 hole_score = []
 
 list1, list2 = [list(map(int, input().split(","))) for _ in range(2)]
-for score_1, score_2 in zip(list1, list2):
-    score = score_1 - score_2
+for hole, score in zip(list1, list2):
+    SCORE_MAP = {-4: "コンドル", -3: "アルバトロス", -2: "イーグル", -1: "バーディ", 0: "パー",
+                 1: "ボギー"}
 
-    if score_1 <= 4 and score_2 == 1:
+    result = score - hole
+
+    if hole <= 4 and score == 1:
         hole_score.append("ホールインワン")
-    elif score_1 == 5 and score_2 == 1:
-        hole_score.append("コンドル")
-    elif score_1 == 5 and score_2 == 2:
-        hole_score.append("アルバトロス")
-    elif score_2 == score_1 - 2:
-        hole_score.append("イーグル")
-    elif score_2 == score_1 - 1:
-        hole_score.append("バーディ")
-    elif score_1 == score_2:
-        hole_score.append("パー")
-    elif score < 0:
-        hole_score.append("ボギー" if score == -1 else f"{-score}ボギー")
-    else:
-        print("score_1は3 <= score_1 <= 5, score_2は,1 <= score_1で入力して下さい")
+        continue
+    elif result >= 2:
+        hole_score.append(f"{result}ボギー")
+        continue
+    game_score = SCORE_MAP.get(result)
+    hole_score.append(game_score)
 
 print(",".join(hole_score))
